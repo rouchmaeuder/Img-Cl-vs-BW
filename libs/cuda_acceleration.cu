8d63901de33b1bc9f77807f478dcfdaea7d757f7
rouchmaeuder <leonardo.roschi@gmail.com>
2024-12-05 22:53:33 +0100

    compiles and kinda works, resultpixel calculation in kernel not correct.

--- libs/cuda_acceleration.cu
+++ libs/cuda_acceleration.cu
@@ -3,12 +3,15 @@
 #include "math.h"
 #include "cuda_acceleration.h"
 
+#define BLOCKS		1024
+#define THREADS		1024
+
 // compile with  /usr/local/cuda/bin/nvcc /home/user/tiff_file_parser/libs/cuda_acceleration.cu -o /home/user/tiff_file_parser/cuda_acceleration.o -c
 
 __device__ static inline signed long limit(signed long input, signed long lower, signed long upper);
 __global__ void ParalellTotalContrastParallelismFunction (float **InputData, float **outputData, unsigned int frame, unsigned long index, unsigned int hResolution, unsigned int vResolution);
 float cppParalellTotalContrast(float **image, float radius, unsigned int vResolution, unsigned int hResolution);
-/*void printStatusBar(unsigned char input)
+void printStatusBar(unsigned char input)
 {
 	printf("[");
 	for (unsigned int i = 0; i < 50; i++)
@@ -23,7 +26,8 @@ float cppParalellTotalContrast(float **image, float radius, unsigned int vResolu
 		}
 	}
 	printf("]");
-}*/
+	fflush(stdout);
+}
 
 extern "C"{
 float ParalellTotalContrast(float **image, float radius, unsigned int vResolution, unsigned int hResolution)
@@ -36,22 +40,26 @@ float cppParalellTotalContrast(float **image, float radius, unsigned int vResolu
 {
 	unsigned int frame = radius * hResolution;
 	float returnval = 0;
-	printf("calculating avg contrast: \n");
-
-	unsigned long index = 0;
-
 	float ** cu_image;
 	float ** output;
+	unsigned long index = 0;
 
+	printf("calculating avg contrast: \n");
+	printf("trying to allocate %i bytes of type float with sizeof %li resulting in %li bytes allocated \n", hResolution, sizeof(float*), sizeof(float*) * hResolution);
 
-	cudaMallocManaged(&cu_image, sizeof(float*) * hResolution); //setup 1st dimention input array
+	if(cudaSuccess != cudaMallocManaged(&cu_image, sizeof(float*) * hResolution)) //setup 1st dimention input array
+	{
+		printf("allocation Error\n");
+		return 0;
+	}
+	printf("malloced first\n");
 
 	for(unsigned int i = 0; i < hResolution; i++) // allocate second dimention storage
 	{
 		cudaMallocManaged(&cu_image[i], sizeof(float) * vResolution);
 	}
 
-	printf("cuda image array allocated");
+	printf("cuda image array allocated\n");
 
 	for (unsigned int x = 0; x < hResolution; x++)
 	{
@@ -69,22 +77,26 @@ float cppParalellTotalContrast(float **image, float radius, unsigned int vResolu
 	{
 		cudaMallocManaged(&output[i], sizeof(float) * vResolution);
 	}
-	printf("cuda outputarr allocated");
+	printf("cuda outputarr allocated\n");
+
+//	unsigned char exitflag = 0;
 
 	while(index < (vResolution * hResolution))
 	{
-		if(((vResolution * hResolution) - index) < 512)
+		printf("%li, ", index);
+		fflush(stdout);
+		if(((vResolution * hResolution) - index) < (BLOCKS * THREADS))
 		{
 			ParalellTotalContrastParallelismFunction <<<(((vResolution * hResolution) - index)),(1)>>> (cu_image, output, (radius * hResolution), index, hResolution, vResolution);
 			index += ((vResolution * hResolution) - index);
 		}
 		else
 		{
-			ParalellTotalContrastParallelismFunction <<<(512),(1)>>> (cu_image, output, (radius * hResolution), index, hResolution, vResolution);
-			index += 512;
+			ParalellTotalContrastParallelismFunction <<<(THREADS),(BLOCKS)>>> (cu_image, output, (radius * hResolution), index, hResolution, vResolution);
+			index += (BLOCKS * THREADS);
 		}
-//		printf("\r");
-//		printStatusBar((unsigned char)(((float)index / (float)(hResolution * vResolution)) * 100));
+		printf("\r");
+		printStatusBar((unsigned char)(((float)index / (float)(hResolution * vResolution)) * 100));
 		cudaDeviceSynchronize();
 	}
 
