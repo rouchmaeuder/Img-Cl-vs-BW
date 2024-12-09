0be34c115e71fc89e434942b1cc60f75b6a7425f
rouchmaeuder <leonardo.roschi@gmail.com>
2024-12-09 19:48:44 +0100

    takes a folder and evaluates all contained imgs and stored their outputs to csv

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "libs/cuda_acceleration.h"
 #include "time.h"
 #include "dirent.h"
+#include "string.h"
 #include "libs/tiff.h"
 
 // compile with gcc main.c -lm -o main.o 
@@ -10,6 +11,8 @@
 #define ANSI_RESET "\x1b[0m"
 #define ANSI_WHITE_BKGRND "\x1b[30;107m"
 
+#define OUTPUTFILENAME "outputs.csv"
+
 void printStatusBar(unsigned char input);																					   // takes a unsigned char from 0 to 100 as a percentage
 static inline signed long limit(signed long input, signed long lower, signed long upper);
 
@@ -18,33 +21,70 @@ float totalContrast(float **image, float radius); // calculate contrast
 int main(int argc, char * argv[])
 {
 	VerboseFlag = PrintNone;
-/*	DIR * imgDir = opendir(argv[1]);
 
-	if (imgDir == NULL)
+	if(argc <= 1)
 	{
-		printf("was not able to open directory");
-		return 0;
-	}*/
-	
-
-	struct tiff img;
-	enum errorType dbgError = openTiff(&img, 1, "/home/user/tiff_file_parser/data/pic.tif");
-	if (dbgError != Success)
-	{
-		printf("error%s\n", errConvertToString(dbgError));
 		return 1;
 	}
 
-	printf("evaluating with graphicscard\n");
-	time_t seconds = time(NULL);
-//	printf("total contrast is %f\n", ParalellTotalContrast(img.BW_Data, 0.01, img.vResolution, img.hResolution));
-	time_t seconds_ref = time(NULL);
-	printf("took %li seconds\n", seconds_ref - seconds);
+	DIR * imgDir = opendir(argv[1]);
 
-	printPreview(&img, 32);
+	if (imgDir == NULL)
+	{
+		printf("was not able to open directory\n");
+		return 0;
+	}
+	printf("directory opened\n");
 
-	closeTiff(&img);
-//	closedir(imgDir);
+	struct dirent *filepath;
+	unsigned int filenum = 0;
+	char** filePathArr = NULL;
+
+	filepath = readdir(imgDir);
+	while (filepath != NULL)
+	{
+		if((strstr(filepath->d_name, ".tif") != NULL) && (filepath->d_type == DT_REG)) // search folder for .tif files and store their paths in filePathArr. store the amount of files found in filenum
+		{
+			if(filenum)
+			{
+				filePathArr = realloc(filePathArr, sizeof(char*) * (1 + filenum));
+			}
+			else
+			{
+				filePathArr = malloc(sizeof(char*));
+			}
+			
+			filePathArr[filenum] = malloc(sizeof(filepath->d_name) + sizeof(argv[1]));
+			strcpy(filePathArr[filenum], argv[1]);
+			strcat(filePathArr[filenum], filepath->d_name);
+			printf("%s\n", filePathArr[filenum]);
+
+			filenum++;
+		}
+		filepath = readdir(imgDir);
+	}
+
+	char* outputFilePath = malloc(sizeof(argv[1]) + sizeof(OUTPUTFILENAME));
+	strcpy(outputFilePath, argv[1]);
+	strcat(outputFilePath, OUTPUTFILENAME);
+	FILE *outFile = fopen(outputFilePath, "w+"); // create output .csv file
+	free(outputFilePath);
+
+	struct tiff img;
+	for (unsigned char i = 0; i < filenum; i++) // loop over all filepaths
+	{
+		openTiff(&img, 1, filePathArr[i]); // open the image
+		float contrastVal = ParalellTotalContrast(img.BW_Data, 0.01, img.vResolution, img.hResolution); // analyze the image
+		closeTiff(&img); // close the image
+
+		printf("total contrast is %f\n", contrastVal); // print the resolution to console
+		fprintf(outFile,"%s, %f \n", filePathArr[i], contrastVal); // print result to file
+		free(filePathArr[i]); // free the filepath array
+	}
+
+	fclose(outFile);
+	free(filePathArr);
+	closedir(imgDir);
 	return 0;
 }
 
